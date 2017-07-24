925bb410dc64c623f66f7668dc17d98ec566a511
Michael Vaouli <mykelxknight@Michaels-Mini.home>
2017-07-23 19:34:52 -0700

    update to test_triangle

--- python/researchDev/test_triangle.py
+++ python/researchDev/test_triangle.py
@@ -1,14 +1,9 @@
 from unittest import TestCase
-from Triangle import Triangle
+from triangle import Triangle
 
-
-class TestTriangle2(TestCase):
+class TestTriangle(TestCase):
     def test_Triangle(self):
         self.assertEqual(Triangle(4, 8), 16)
         self.assertEqual(Triangle(6, 2), 6)
         self.assertEqual(Triangle(2, 4), 4)
         self.fail()
-
-
-if __name__ == '__main__':
-    unittest.main()
