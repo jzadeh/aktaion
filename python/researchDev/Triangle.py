dbe4a5bb40c8aa2ebf501f9235cbad84600b9f79
Michael Vaouli <mykelxknight@Michaels-Mini.home>
2017-07-23 18:34:51 -0700

    Testing Simple Unit Test

---
+++ python/researchDev/Triangle.py
@@ -0,0 +1,21 @@
+# x = x xor y
+# y = x xor y
+# x = x xor y
+
+def Triangle(base, height):
+    """Return the area of Triangle"""
+    area = 0.5 * base * height
+    return area
+
+
+base = raw_input("Enter triangle base length: ")
+height = raw_input("Enter triangle height: ")
+
+base = float(base)
+height = float(height)
+
+print 'Area of triangle is', Triangle(base, height)
+
+area = Triangle(base, height)
+print 'The area of the triangle is', area
+
